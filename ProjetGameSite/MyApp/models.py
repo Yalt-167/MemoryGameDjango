3e65686e2b6450c43029c5460b0f647b3370530b
Yalt-167 <107402939+Yalt-167@users.noreply.github.com>
2023-12-01 14:18:50 +0100

    .venv finally?

--- ProjetGameSite/MyApp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Users(models.Model):
-    userEmail = models.CharField(max_length=255)
-    userName = models.CharField(max_length=255)
-    userPrivacy = models.BooleanField() # wether his history is public (True) or only visible to friends (False)
--- ProjetGameSite/MyApp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.Home, name="Home"),
-    path("", views.DisplayUsers, name="DisplayUsers"),
-]
