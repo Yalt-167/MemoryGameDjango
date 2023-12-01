b1f714197a2d7702ba540d169fc3804d27cab75c
Yalt-167 <107402939+Yalt-167@users.noreply.github.com>
2023-12-01 12:13:49 +0100

    I wanna die

---
+++ ProjetGameSite/MyApp/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+
+class Users(models.Model):
+    userEmail = models.CharField(max_length=255)
+    userName = models.CharField(max_length=255)
