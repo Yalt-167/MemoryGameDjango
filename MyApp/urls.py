db0b74825710f7bcae879672bfdf7ac5e8124622
Yalt-167 <107402939+Yalt-167@users.noreply.github.com>
2023-12-01 16:41:27 +0100

    azertyuiop

--- MyApp/urls.py
+++ MyApp/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.Home, name="Home"),
-    path("", views.DisplayUsers, name="DisplayUsers"),
+    path("DisplayUsers", views.DisplayUsers, name="DisplayUsers"),
+    path('', MemoryGameView.as_view(), name='memory_game'),
+    path('flip_card/<int:card_id>/', flip_card, name='flip_card')
 ]
--- MyApp/views.py
+++ MyApp/views.py
@@ -1,10 +1,35 @@
-from django.shortcuts import render, HttpResponse
-from .models import Users
+from django.shortcuts import render, HttpResponse, redirect
+from django.views import View
+from .models import Card
+import random as rdm
 # Create your views here.
 
 def Home(request):
     return render(request, "home.html")
 
 def DisplayUsers(request):
-    items = Users.objects.all()
+    # items = Users.objects.all()
     return render(request, "DisplayUsers.html", {"users": items})
+
+
+class MemoryGameView(View):
+    template_name = 'game/memory_game.html'
+
+    def get(self, request):
+        cards = Card.objects.all()
+
+        # Shuffle the cards
+        card_list = list(cards)
+        random.shuffle(card_list)
+
+        return render(request, self.template_name, {'cards': card_list})
+
+
+def flip_card(request, card_id):
+    card = Card.objects.get(pk=card_id)
+
+    if not card.paired:
+        card.paired = True
+        card.save()
+
+    return redirect('memory_game')
