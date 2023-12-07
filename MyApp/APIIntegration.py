41ea0d9aecf06dcc5675f2b48bce9aef89cd63c7
Yalt-167 <107402939+Yalt-167@users.noreply.github.com>
2023-12-07 01:59:35 +0100

    sanity: 404 not found
    Searching for the remains of my sanity

--- MyApp/APIIntegration.py
+++ MyApp/APIIntegration.py
@@ -1,5 +1,6 @@
 # import requests
 
+<<<<<<< Updated upstream
 # URL_ = "https://api.memegen.link/images/custom/" # _ at the end bc don t wanna risk overwriting anything remotely important
 # def generate_meme(topText: str, bottomText: str):
 
@@ -14,5 +15,23 @@
 #             file.write(response.content)
 #     else:
 #         print(f"Failed. Status code: {response.status_code}")
+=======
+URL_ = "https://api.memegen.link/images/custom/"
+# _ at the end bc don t wanna risk overwriting anything remotely important
+
+def generate_meme(topText: str, bottomText: str):
+
+    #links cant contain spaces so replace them with _
+    memeURL = f"{URL_}{topText.replace(" ", "_")}/{bottomText.replace(" ", "_")}.png"
+
+    # download the meme image
+    response = requests.get(memeURL)
+
+    if response.status_code == 200:
+        with open("meme.png", "wb") as file: # wb => write binary
+            file.write(response.content)
+    else:
+        print(f"Failed. Status code: {response.status_code}")
+>>>>>>> Stashed changes
 
 # generate_meme("Hello", "World")
