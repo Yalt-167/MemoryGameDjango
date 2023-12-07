ef8ce26b272890061a6e22aa2450b551dd4056e5
Yalt-167 <107402939+Yalt-167@users.noreply.github.com>
2023-12-07 02:00:28 +0100

    fixed it

--- MyApp/APIIntegration.py
+++ MyApp/APIIntegration.py
@@ -1,21 +1,5 @@
-# import requests
+import requests
 
-<<<<<<< Updated upstream
-# URL_ = "https://api.memegen.link/images/custom/" # _ at the end bc don t wanna risk overwriting anything remotely important
-# def generate_meme(topText: str, bottomText: str):
-
-#     #links cant hold spaces so replace them with _
-#     memeURL = f"{URL_}{topText.replace(" ", "_")}/{bottomText.replace(" ", "_")}.png"
-
-#     # Download the meme image
-#     response = requests.get(memeURL)
-
-#     if response.status_code == 200:
-#         with open("meme.png", "wb") as file: # wb = write binary
-#             file.write(response.content)
-#     else:
-#         print(f"Failed. Status code: {response.status_code}")
-=======
 URL_ = "https://api.memegen.link/images/custom/"
 # _ at the end bc don t wanna risk overwriting anything remotely important
 
@@ -32,6 +16,5 @@ def generate_meme(topText: str, bottomText: str):
             file.write(response.content)
     else:
         print(f"Failed. Status code: {response.status_code}")
->>>>>>> Stashed changes
 
 # generate_meme("Hello", "World")
