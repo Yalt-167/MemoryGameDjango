8a8e59e516f9fab2d65d7b18c6c48023433b64be
Yalt-167 <107402939+Yalt-167@users.noreply.github.com>
2023-12-08 00:38:25 +0100

    aze

--- MyApp/views.py
+++ MyApp/views.py
@@ -44,7 +44,12 @@ def Login(request):
     # If the data is same in the database and in the connexion page redirecting toward  the next page
         if user is not None:
             login(request,user)
-            [Score(user=request.user, score_value=rdm.randint(0, 100), timestamp=datetime.datetime.now()).save() for _ in range(rdm.randint(3, 10))]
+            [
+                Score(
+                    user=request.user,
+                    score_value=rdm.randint(0, 100),
+                    timestamp=datetime.datetime.now()).save() for _ in range(rdm.randint(3, 10))
+                    ]
             return HomePage(request)
             # redirect vers Memory.html
         else:
@@ -81,7 +86,7 @@ def LoginPage(request):
     return render(request, "Login.html")
 
 def LeaderboardPage(request):
-    topScores = Score.objects.order_by("-score_value")[:10]
+    topScores = Score.objects.order_by("-score_value")[:10][::-1]
     return render(request, "Leaderboards.html", {"topScores": topScores})
 
 
