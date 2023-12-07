71602b0d7cc93e9c8cb7e8131dc1f7929603d2df
Patania <ivaniatavaressilva@gmail.com>
2023-12-07 22:52:09 +0100

    OU EST LE SAPERLIPOPPETE DE USER ID

--- MyApp/models.py
+++ MyApp/models.py
@@ -2,14 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# Create your models here
+# Create your models hedre
 
 class UserHistory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True, primary_key=True)
     score = models.IntegerField(default=0)
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     history = UserHistory()
     bio = models.TextField(blank=True)
     # profilePicture = models.ImageField(upload_to='profile_pics/', blank=True) // if we get to bonus parts
